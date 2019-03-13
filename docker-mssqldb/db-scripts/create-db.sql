7c66bcef1a2fddeb07e95bcac07ecf2b8c00db1f
Jean <jcaferreira@scopus.com.br>
2019-03-13 17:41:55 -0300

    Correções

--- docker-mssqldb/db-scripts/create-db.sql
+++ docker-mssqldb/db-scripts/create-db.sql
@@ -1,45 +1,95 @@
 USE [master]
 GO
-CREATE LOGIN [usr_app] WITH PASSWORD=N'usr_app', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+
+IF NOT EXISTS (
+		SELECT TOP 1 1
+		FROM master.dbo.syslogins
+		WHERE NAME = 'usr_app'
+		)
+BEGIN
+	CREATE LOGIN [usr_app]
+		WITH PASSWORD = N'usr_app'
+			,DEFAULT_DATABASE = [master]
+			,CHECK_EXPIRATION = OFF
+			,CHECK_POLICY = OFF
+END
 GO
-if not exists(select top 1 1 from sysdatabses where name = 'DB1')
-   BEGIN
-      CREATE DATABASE [DB1]
-   END
+
+IF NOT EXISTS (
+		SELECT TOP 1 1
+		FROM master.dbo.sysdatabases
+		WHERE NAME = 'DB1'
+		)
+BEGIN
+	CREATE DATABASE [DB1]
+END
 GO
-if not exists(select top 1 1 from sysdatabses where name = 'DB2')
-   BEGIN
-      CREATE DATABASE [DB2]
-   END
+
+IF NOT EXISTS (
+		SELECT TOP 1 1
+		FROM master.dbo.sysdatabases
+		WHERE NAME = 'DB2'
+		)
+BEGIN
+	CREATE DATABASE [DB2]
+END
 GO
+
 USE [db1]
 GO
-if NOT exists(select top 1 1 from sysusers where name like 'usr_app')
-	BEGIN
-		CREATE USER [usr_app] FOR LOGIN [usr_app]
-		ALTER ROLE [db_owner] ADD MEMBER [usr_app]
-	END
+
+IF NOT EXISTS (
+		SELECT TOP 1 1
+		FROM sysusers
+		WHERE NAME LIKE 'usr_app'
+		)
+BEGIN
+	CREATE USER [usr_app]
+	FOR LOGIN [usr_app]
+
+	ALTER ROLE [db_owner] ADD MEMBER [usr_app]
+END
 ELSE
-	BEGIN
-		EXEC SP_CHANGE_USERS_LOGIN 'UPDATE_ONE','usr_app','usr_app';
-	END
+BEGIN
+	EXEC SP_CHANGE_USERS_LOGIN 'UPDATE_ONE'
+		,'usr_app'
+		,'usr_app';
+END
 GO
+
 USE MASTER
 GO
-if not exists(select top 1 1 from sysdatabses where name = 'DB_FATE_HOMOLOG')
-   BEGIN
-      CREATE DATABASE [DB_FATE_HOMOLOG]
-   END
+
+IF NOT EXISTS (
+		SELECT TOP 1 1
+		FROM master.dbo.sysdatabases
+		WHERE NAME = 'DB_FATE_HOMOLOG'
+		)
+BEGIN
+	CREATE DATABASE [DB_FATE_HOMOLOG]
+END
 GO
+
 USE [DB_FATE_HOMOLOG]
 GO
-if NOT exists(select top 1 1 from sysusers where name like 'usr_app')
-	BEGIN
-		CREATE USER [usr_app] FOR LOGIN [usr_app]
-		ALTER ROLE [db_owner] ADD MEMBER [usr_app]
-	END
+
+IF NOT EXISTS (
+		SELECT TOP 1 1
+		FROM sysusers
+		WHERE NAME LIKE 'usr_app'
+		)
+BEGIN
+	CREATE USER [usr_app]
+	FOR LOGIN [usr_app]
+
+	ALTER ROLE [db_owner] ADD MEMBER [usr_app]
+END
 ELSE
-	BEGIN
-		EXEC SP_CHANGE_USERS_LOGIN 'UPDATE_ONE','usr_app','usr_app';
-	END
+BEGIN
+	EXEC SP_CHANGE_USERS_LOGIN 'UPDATE_ONE'
+		,'usr_app'
+		,'usr_app';
+END
 GO
+
+
