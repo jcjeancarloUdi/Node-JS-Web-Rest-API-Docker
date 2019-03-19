42fe3da9654d63d8be1f084cb363265775c2ac9f
jcjeancarloudi <jcjeancarlo@gmail.com>
2019-03-18 22:19:41 -0300

    Correções

--- mssql-restapi-nodejs/database/create-table.js
+++ mssql-restapi-nodejs/database/create-table.js
@@ -11,36 +11,16 @@ function createTable(conn, res){
     table.columns.add('ID', sql.Int, {nullable: false, primary: true});
     table.columns.add('Nome', sql.NVarChar(150), {nullable: false});
     table.columns.add('CPF', sql.NChar(11), {nullable: false});
-    table.rows.add(1, 'Duerpo', '5689131830');
-    table.rows.add(2, 'Gyalosas', '16081025802');
-    table.rows.add(3, 'Erteimph', '29515354811');
-    table.rows.add(4, 'Linrod', '31793134804');
-    table.rows.add(5, 'Lyesi', '16076482885');
-    table.rows.add(6, 'Lugusgarn', '48508110863');
-    table.rows.add(7, 'Hurim', '45962030834');
-    table.rows.add(8, 'Vyolor', '14828049800');
-    table.rows.add(9, 'Guobe', '2806044839');
-    table.rows.add(10, 'Lumdlufa', '15376249891');
-    table.rows.add(11, 'Fyurond', '60084103868');
-    table.rows.add(12, 'Pahtici', '32545185868');
-    table.rows.add(13, 'Khadpug', '1598058800');
-    table.rows.add(14, 'Adanveythir', '1590464834');
-    table.rows.add(15, 'Nursauweu', '22001870841');
-    table.rows.add(16, 'Ceralond', '13187252834');
-    table.rows.add(17, 'Teislob', '15449424868');
-    table.rows.add(18, 'Hikavo', '1596064897');
-    table.rows.add(19, 'Bogrla', '2820332897');
-    table.rows.add(20, 'Rimuthic', '13774190887');
-    table.rows.add(21, 'Voges', '32984513800');
-    table.rows.add(22, 'Govoible', '39268900882');
-    table.rows.add(23, 'Fuxerandir', '31798616815');
-    table.rows.add(24, 'Malwudir', '29147514868');
-    table.rows.add(25, 'Ulmoauil', '18474195853');
-    table.rows.add(26, 'Anbruan', '3323080815');
-    table.rows.add(27, 'Geinho', '5640520868');
-    table.rows.add(28, 'Zoepile', '1596934891');
-    table.rows.add(29, 'Dasu', '31792286872');
-    table.rows.add(30, 'Belmuko', '19020341804');
+    table.rows.add(1, 'Luan Bryan Ricardo Teixeira', '65392215173');
+    table.rows.add(2, 'Mateus Yago Breno das Neves', '49912710100');
+    table.rows.add(3, 'Carlos Diogo Cláudio Araújo', '28497497104');
+    table.rows.add(4, 'Pedro Calebe Osvaldo Costa', '80245867112');
+    table.rows.add(5, 'Joaquim Raul Peixoto', '82118876149');
+    table.rows.add(6, 'Luan Cauê Assunção', '74501257148');
+    table.rows.add(7, 'Cláudio Bryan Henry da Silva', '42514228131');
+    table.rows.add(8, 'Yago Giovanni Souza', '48389524147');
+    table.rows.add(9, 'Eduardo Roberto Assis', '97513028117');
+    table.rows.add(10, 'Manoel Raimundo Moreira', '62964155160');
   
 
     const request = new sql.Request()
